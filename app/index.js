85f01e2170a7dcf6115eb59cd26fb64227503e09
umut9191 <umutsurmeli91@gmail.com>
2023-09-04 23:36:36 +0300

    starter assets ,styles,constanst and components added to project

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View,Text } from "react-native";
+
+import React, { Component } from 'react'
+
+const Home = () => {
+    return (
+      <View>
+        <Text>Home</Text>
+      </View>
+    )
+}
+
+export default Home;
